29eab71c44350019cbf2a3d6a7de798b3d2cb9fc
Nihar Patel <nihar7540@gmail.com>
2026-07-17 14:48:39 +0530

    Update 3.4.py

--- APP LABWORK & SELF PRAC,,,/3.4.py
+++ APP LABWORK & SELF PRAC,,,/3.4.py
@@ -1,48 +1,48 @@
-# for i in range(1,20+1):
-#     if i%4==0:
-#         continue
-#     print(i)
+for i in range(1,20+1):
+    if i%4==0:
+        continue
+    print(i)
 
-# #######################################
+#######################################
 
-# for i in range (1,10+1):
-#     if i==7:
-#         break
-#     print(i)
+for i in range (1,10+1):
+    if i==7:
+        break
+    print(i)
 
-# ########################################
-# a="PYTHON"
-# for i in a:
-#     if i in "aeiouAEIOU":
-#         continue
-#     print(i)
+########################################
+a="PYTHON"
+for i in a:
+    if i in "aeiouAEIOU":
+        continue
+    print(i)
     
-# #########################################
+#########################################
 
-# num=int(input("enter range for multiplication :"))
-# for i  in range(1,num+1):
-#     for j in range(1,10+1):
-#         table=i*j
-#         print(f"{i} * {j} = {table}")
+num=int(input("enter range for multiplication :"))
+for i  in range(1,num+1):
+    for j in range(1,10+1):
+        table=i*j
+        print(f"{i} * {j} = {table}")
     
-#     print("--------------------------------")
+    print("--------------------------------")
 
-# #########################################
-# n=5
-# for i in range(1,n+1):
-#     for j in range(1,i+1):
-#         print(j,end="")
-#     print("")    
+#########################################
+n=5
+for i in range(1,n+1):
+    for j in range(1,i+1):
+        print(j,end="")
+    print("")    
 
-# #########################################
+#########################################
 
-# for i in range(5, 0, -1):
-#     for j in range(5, i - 1, -1):
-#         print(j, end="")
-#     print()  
+for i in range(5, 0, -1):
+    for j in range(5, i - 1, -1):
+        print(j, end="")
+    print()  
 
 
-##########################################
+#########################################
 
 for i in range(0,5):
     for j in range(i+1,5+1):
